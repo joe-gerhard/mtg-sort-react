60789b1c0065957914354fe99917ecd081fe056b
Joe Gerhard <jlgerhard@gmail.com>
2019-10-29 22:13:35 -0500

    Add card enter/exit animations

--- src/components/CardDisplay/CardDisplay.jsx
+++ src/components/CardDisplay/CardDisplay.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { StyledCardDisplay, StyledImg } from "./styles";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const CardDisplay = ({ cards, filter }) => {
   return (
     <StyledCardDisplay>
-      {cards
-        ? cards.map(card => {
-          let filtered = false;
+      <TransitionGroup>
+        {cards &&
+          cards.map(card => {
+            let filtered = false;
             card.colors.forEach(color => {
-              if (!filtered) filtered = filter[color]
-            })
+              if (!filtered) filtered = filter[color];
+            });
             if (card.colors.length === 0 && filter.Colorless) filtered = true;
 
             return (
-              (card.imageUrl && !card.type.includes("Adventure") && !filtered) && (
-                <StyledImg key={card.id} src={card.imageUrl} alt={card.name} />
+              card.imageUrl &&
+              !card.type.includes("Adventure") &&
+              !filtered && (
+                <CSSTransition key={card.id} timeout={500} classNames="card">
+                  <StyledImg
+                    key={card.id}
+                    src={card.imageUrl}
+                    alt={card.name}
+                  />
+                </CSSTransition>
               )
             );
-          })
-        : "loading..."}
+          })}
+      </TransitionGroup>
     </StyledCardDisplay>
   );
 };
--- src/components/CardDisplay/styles.js
+++ src/components/CardDisplay/styles.js
@@ -5,11 +5,42 @@ export const StyledCardDisplay = styled.div(({theme, ...props}) => css`
   justify-content: center;
   flex-wrap: wrap;
   margin-top: .5vw;
+  
+  .card-enter {
+    opacity: 0;
+    width: 0px;
+    margin: .5vw 0px;
+    object-fit: fill;
+  }
+
+  .card-enter-active {
+    opacity: 1;
+    transition: all .4s;
+    width: 15vw;
+    object-fit: fill;
+    margin: .5vw;
+  }
+
+  .card-exit {
+    opacity: 1;
+    width: 15vw;
+    object-fit: fill;
+    margin: .5vw;
+  }
+
+  .card-exit-active {
+    opacity: 0;
+    transition: all .4s;
+    width: 0px;
+    object-fit: fill;
+    margin: .5vw 0;
+  }
 `)
 
 export const StyledImg = styled.img(({theme, ...props}) => css`
   margin: .5vw;
+  height: 20vw;
   width: 15vw;
-  object-fit: scale-down;
   filter: drop-shadow(.3vw .3vw .2vw rgba(17, 25, 58, 0.8));
+  object-fit: fill;
 `)
